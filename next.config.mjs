c94bc75d26b426c8f9bdc32b051b8512f45eddfd
Boris <47457848+boris-chb@users.noreply.github.com>
2023-06-09 13:51:31 +0100

    basic UI

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "abs.twimg.com",
+        port: "",
+        pathname: "/sticky/**",
+      },
+    ],
+  },
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,216 @@
+import {
+  AdjustmentsHorizontalIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  FaceSmileIcon,
+  MapPinIcon,
+  PhotoIcon,
+} from "@heroicons/react/24/solid";
+import {
+  ChatBubbleOvalLeftEllipsisIcon,
+  ArrowUpTrayIcon,
+  HeartIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/24/outline";
+
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import Image from "next/image";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import { api } from "~/utils/api";
+
+interface TweetProps {
+  tweet: {
+    author: {
+      name: string;
+      handle: string;
+    };
+    body: string;
+    createdAt: Date;
+    likes: number;
+    retweets: number;
+  };
+}
+
+const tweets = [
+  {
+    id: 1,
+    author: {
+      name: "John Doe",
+      handle: "johndoe",
+    },
+    createdAt: new Date("2022-01-01T12:00:00Z"),
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    likes: 42,
+    retweets: 12,
+  },
+  // Add more tweets here
+];
+
+const CreateTweet = () => {
+  const [tweetText, setTweetText] = useState("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTweetText(e.target.value);
+  };
+
+  const handleAttachMedia = () => {
+    // Handle attaching media logic here
+    console.log("Attach media");
+  };
+
+  const handleAttachGif = () => {
+    // Handle attaching GIF logic here
+    console.log("Attach GIF");
+  };
+
+  const handleAttachPoll = () => {
+    // Handle attaching poll logic here
+    console.log("Attach poll");
+  };
+
+  const handleAttachLocation = () => {
+    // Handle attaching location logic here
+    console.log("Attach location");
+  };
+
+  const handleScheduleTweet = () => {
+    // Handle scheduling tweet logic here
+    console.log("Schedule tweet");
+  };
+
+  const handleSubmitTweet = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle submitting tweet logic here
+    console.log("Tweet submitted:", tweetText);
+    // setTweetText('');
+  };
+
+  return (
+    <div className="flex rounded-lg bg-white p-4 shadow-md">
+      <div className="mr-4">
+        <Image
+          className="h-12 w-12 rounded-full"
+          src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+          alt="Profile Picture"
+          width={400}
+          height={400}
+        />
+      </div>
+      <form className="flex-grow" onSubmit={handleSubmitTweet}>
+        <textarea
+          className="w-full resize-none bg-transparent outline-none"
+          rows={3}
+          placeholder="What's happening?"
+          value={"new tweet"}
+          onChange={handleInputChange}
+        />
+        <div className="mt-2 flex items-center justify-between">
+          <div className="flex space-x-4">
+            <button
+              title="Attach Media"
+              type="button"
+              onClick={handleAttachMedia}
+            >
+              <PhotoIcon className="h-6 w-6 text-blue-500 hover:text-blue-600" />
+            </button>
+            <button title="Attach GIF" type="button" onClick={handleAttachGif}>
+              <FaceSmileIcon className="h-6 w-6 text-blue-500 hover:text-blue-600" />
+            </button>
+            <button
+              title="Attach poll"
+              type="button"
+              onClick={handleAttachPoll}
+            >
+              <ChartBarIcon className="h-6 w-6 text-blue-500 hover:text-blue-600" />
+            </button>
+            <button
+              title="Add location"
+              type="button"
+              onClick={handleAttachLocation}
+            >
+              <MapPinIcon className="h-6 w-6 text-blue-500 hover:text-blue-600" />
+            </button>
+            <button
+              title="Schedule tweet"
+              type="button"
+              onClick={handleScheduleTweet}
+            >
+              <CalendarIcon className="h-6 w-6 text-blue-500 hover:text-blue-600" />
+            </button>
+          </div>
+          <button
+            type="submit"
+            className="rounded-3xl bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          >
+            Tweet
+          </button>
+        </div>
+      </form>
+      <AdjustmentsHorizontalIcon className="ml-4 h-6 w-6 text-gray-500 hover:text-blue-500" />
+    </div>
+  );
+};
+
+const Tweet: React.FC<TweetProps> = ({
+  tweet: { author, body, createdAt, likes, retweets },
+}) => {
+  return (
+    <div className="flex border-b border-gray-200 p-4">
+      <Image
+        className="mr-4 h-12 w-12 rounded-full"
+        src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+        alt="Profile Picture"
+        width={400}
+        height={400}
+      />
+      <div className="flex-grow">
+        <div className="flex items-center">
+          <span className="mr-2 font-bold">{author.name}</span>
+          <span className="text-gray-500">
+            @<span>{author.handle}</span>
+          </span>
+          <span className="mx-2 text-gray-500">•</span>
+          <span className="text-gray-500">2h</span>
+        </div>
+        <div>{body}</div>
+        <div className="mt-4 flex w-full items-center justify-between gap-5">
+          <button
+            title="Comment"
+            className="text-gray-500 hover:text-blue-500 focus:outline-none"
+          >
+            <div className="flex"></div>
+            <ChatBubbleOvalLeftEllipsisIcon className="mr-1 h-5 w-5" />
+          </button>
+          <button
+            title="Retweet"
+            className="text-gray-500 hover:text-green-500 focus:outline-none"
+          >
+            <div className="flex">
+              <ArrowUpTrayIcon className="mr-1 h-5 w-5" />
+              {retweets}
+            </div>
+          </button>
+          <button
+            title="Likes"
+            className="text-gray-500 hover:text-red-500 focus:outline-none"
+          >
+            <div className="flex">
+              <HeartIcon className="mr-1 h-5 w-5" />
+              {likes}
+            </div>
+          </button>
+          <button
+            title="Share"
+            className="ml-auto text-gray-500 hover:text-gray-700 focus:outline-none"
+          >
+            <EllipsisHorizontalIcon className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -9,42 +218,16 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Create T3 App</title>
-        <meta name="description" content="Generated by create-t3-app" />
+        <title>Twitter Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+      <main className="flex min-h-screen flex-col items-center justify-center gap-3 bg-gray-100">
+        <div className="container mx-auto flex flex-col gap-5 p-4">
+          <CreateTweet />
+
+          {tweets.map((tweet) => (
+            <Tweet key={tweet.id} tweet={tweet} />
+          ))}
         </div>
       </main>
     </>
